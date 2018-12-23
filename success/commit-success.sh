998c5253fd37bb568959c3e16a368122e1010190
sadzeih <sadzeih@gmail.com>
2018-12-23 16:47:30 +0100

    Resolved issues

--- success/commit-success.sh
+++ success/commit-success.sh
@@ -15,11 +15,11 @@ fi
 
 URI=https://api.github.com
 API_VERSION=v3
-API_HEADER="Accept: application/vnd.github.${API_VERSION}+json; application/vnd.github.antiope-preview+json"
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 set_commit_status() {
-	curl -sSL -H "${AUTH_HEADER}" -H ${API_HEADER} -d '{"state": "success"}' -H 'Content-Type: application/json' -X POST "${URI}/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"
+	curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -d '{"state": "success"}' -H "Content-Type: application/json" -X POST "${URI}/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"
 }
 
 set_commit_status
